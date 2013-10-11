bce971f5e19b4c03c15c542fac7860ea322150ee
dfockler <dan@danserv.(none)>
2013-10-11 06:16:27 +0400

    adding rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpokaneRb::Application.config.secret_token = 'b56b829ad8a78224866263b044bfa384a61fde516fa1b8ac599ebc3daa30e6817ca54facf71606a122de7796799909ea277bce27264c6e88e94a4dcbf536ef62'
